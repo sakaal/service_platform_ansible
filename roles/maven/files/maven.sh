fc4f2a1871d583106eb25a5a1474ec2a3004b2c3
Sakari A. Maaranen <sam@iki.fi>
2014-12-13 23:50:57 +0200

    Maven Java SSL trust

--- roles/maven/files/maven.sh
+++ roles/maven/files/maven.sh
@@ -1,4 +1,4 @@
 # /etc/profile.d/maven.sh
 
-MAVEN_OPTS="-Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2"
+MAVEN_OPTS="-Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2 -Djavax.net.ssl.trustStore=/etc/pki/java/cacerts -Djavax.net.ssl.trustStorePassword=changeit"
 export MAVEN_OPTS
